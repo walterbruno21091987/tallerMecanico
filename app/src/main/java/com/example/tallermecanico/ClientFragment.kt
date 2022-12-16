1c737c97e4f65eafd8f9b22a5c56dd30644f1197
walter bruno <walter_bruno21091987@hotmail.com>
2022-12-16 15:45:45 -0300

    tallerMecanicoapk

--- app/src/main/java/com/example/tallermecanico/ClientFragment.kt
+++ app/src/main/java/com/example/tallermecanico/ClientFragment.kt
@@ -5,12 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import com.example.tallermecanico.databinding.FragmentMenuBinding
 
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-class MenuFragment : Fragment() {
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ClientFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ClientFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -27,11 +33,8 @@ class MenuFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        val binding= DataBindingUtil.inflate<FragmentMenuBinding>(inflater,R.layout.fragment_menu,container,false)
         // Inflate the layout for this fragment
-        return binding.root
-
+        return inflater.inflate(R.layout.fragment_client, container, false)
     }
 
     companion object {
@@ -41,12 +44,12 @@ class MenuFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MenuFragment.
+         * @return A new instance of fragment ClientFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MenuFragment().apply {
+            ClientFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
